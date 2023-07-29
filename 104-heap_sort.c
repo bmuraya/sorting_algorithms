b6a8edeb60ef216e0da6753db6e44a9b3cc46565
bmuraya <babumuraya@gmail.com>
2023-07-29 22:15:20 +0300

    mplemented the heap_sort

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -1,41 +1,54 @@
 #include "sort.h"
+
 /**
-  * heap_sort - heap sort algorithm
-  * @array: array to sort
-  * @size: size of array
-  */
+ * heapify - Heapify a subtree rooted at idx in the array
+ * @array: The array to be heapified
+ * @idx: The index of the parent node
+ * @size: The size of the array
+ */
+void heapify(int *array, int idx, size_t size)
+{
+    int max = idx;
+    int left = 2 * idx + 1;
+    int right = 2 * idx + 2;
+    int tmp;
+
+    if (left < size && array[left] > array[max])
+        max = left;
+    if (right < size && array[right] > array[max])
+        max = right;
+    if (max != idx)
+    {
+        tmp = array[idx];
+        array[idx] = array[max];
+        array[max] = tmp;
+        print_array(array, size);
+        heapify(array, max, size);
+    }
+}
+
+/**
+ * heap_sort - Heap sort algorithm
+ * @array: Array to be sorted
+ * @size: Size of the array
+ */
 void heap_sort(int *array, size_t size)
 {
-	int i, tmp;
+    int i, tmp;
 
-	for (i = size / 2 - 1; i >= 0; i--)
-		heapify(array, i, size, size);
-	for (i = size -1; i >=0; i--)
-	{
-		tmp = array[0];
-		array[0] = array[i];
-		array[i] = tmp;
-		print_array(array, size);
-		heapify(array, i, 0, size);
-	}
-}
-void heapify(int *array, int idx, int idx2, size_t size)
-{
-	int max = idx2;
-	int left = 2 * idx2 + 1;
-	int right = 2 * idx2 + 2;
-	int tmp;
+    // Build the heap (rearrange the array)
+    for (i = size / 2 - 1; i >= 0; i--)
+        heapify(array, i, size);
 
-	if (left < idx && array[left] > array[max])
-		max = left;
-	if (right < idx && array[right] > array[max])
-		max = right;
-	if (max != idx2)
-	{
-		tmp = array[idx2];
-		array[idx2] = array[max];
-		array[max] = tmp;
-		print_array(array, size);
-		heapify(array, idx, max, size);
-	}
+    // One by one extract an element from the heap
+    for (i = size - 1; i >= 0; i--)
+    {
+        // Move the current root to the end
+        tmp = array[0];
+        array[0] = array[i];
+        array[i] = tmp;
+
+        // Call heapify on the reduced heap
+        heapify(array, 0, i);
+    }
 }
