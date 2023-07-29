5ea436b78eb0db6fc8b05daaca6254df2883fe67
bmuraya <babumuraya@gmail.com>
2023-07-29 22:44:53 +0300

    betty linter

--- 104-heap_sort.c
+++ 104-heap_sort.c
@@ -8,23 +8,23 @@
  */
 void heapify(int *array, size_t idx, size_t size)
 {
-    size_t max = idx;
-    size_t left = 2 * idx + 1;
-    size_t right = 2 * idx + 2;
-    int tmp;
+size_t max = idx;
+size_t left = 2 * idx + 1;
+size_t right = 2 * idx + 2;
+int tmp;
 
-    if (left < size && array[left] > array[max])
-        max = left;
-    if (right < size && array[right] > array[max])
-        max = right;
-    if (max != idx)
-    {
-        tmp = array[idx];
-        array[idx] = array[max];
-        array[max] = tmp;
-        print_array(array, size);
-        heapify(array, max, size);
-    }
+if (left < size && array[left] > array[max])
+max = left;
+if (right < size && array[right] > array[max])
+max = right;
+if (max != idx)
+{
+tmp = array[idx];
+array[idx] = array[max];
+array[max] = tmp;
+print_array(array, size);
+heapify(array, max, size);
+}
 }
 
 /**
@@ -34,21 +34,21 @@ void heapify(int *array, size_t idx, size_t size)
  */
 void heap_sort(int *array, size_t size)
 {
-    int i, tmp;
+int i, tmp;
 
-    /*Build the heap (rearrange the array)*/
-    for (i = size / 2 - 1; i >= 0; i--)
-        heapify(array, i, size);
+/*Build the heap (rearrange the array)*/
+for (i = size / 2 - 1; i >= 0; i--)
+heapify(array, i, size);
 
-    /*One by one extract an element from the heap*/
-    for (i = size - 1; i >= 0; i--)
-    {
-        /*Move the current root to the end*/ 
-        tmp = array[0];
-        array[0] = array[i];
-        array[i] = tmp;
+/*One by one extract an element from the heap*/
+for (i = size - 1; i >= 0; i--)
+{
+/*Move the current root to the end*/ 
+tmp = array[0];
+array[0] = array[i];
+array[i] = tmp;
 
-        /*Call heapify on the reduced heap*/
-        heapify(array, 0, i);
-    }
+/*Call heapify on the reduced heap*/
+heapify(array, 0, i);
+}
 }
